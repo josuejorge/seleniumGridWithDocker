59bd24ca809e59e24ff2e8b46b6b2fa087e0ab72
jorgeoliveira2099 <josuejorge2099@hotmail.com>
2026-07-03 22:07:09 -0300

    inicial commit com selenium grid e evidencias no testNG report

---
+++ src/test/java/com/vinsguru/listener/TestListener.java
@@ -0,0 +1,30 @@
+package com.vinsguru.listener;
+
+import com.vinsguru.util.Constants;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
+public class TestListener implements ITestListener {
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        logScreenshot(result);
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        logScreenshot(result);
+    }
+
+    private void logScreenshot(ITestResult result) {
+        TakesScreenshot driver = (TakesScreenshot) result.getTestContext().getAttribute(Constants.DRIVER);
+        String screenshot = driver.getScreenshotAs(OutputType.BASE64);
+        String htmlImageFormat = "<img width=700px src='data:image/png;base64,%s' />";
+        String htmlImage = String.format(htmlImageFormat, screenshot);
+        Reporter.log(htmlImage);
+    }
+
+}
